a9926010c78aec8a36fcc1ddf6fc9c034eb39cb7
GarryLau <gan.lau@outlook.com>
2019-09-08 23:29:16 +0800

    Update Command.h

--- Memento/Command.h
+++ Memento/Command.h
@@ -20,31 +20,35 @@ extern "C" {
         }
 
         void execute() {
+            if (m_num_commands >= MAX_OPERATION) {
+                std::cout << "Commands are full." << std::endl;
+                return;
+            }
             m_mem_list[m_num_commands] = m_receiver->createMemento();
             m_command_list[m_num_commands] = this;
-            if (m_num_commands > m_high_water) {
-                m_high_water = m_num_commands;
+            if (m_num_commands > m_maxCommands) {
+                m_maxCommands = m_num_commands;
             }
             m_num_commands++;
             (m_receiver->*m_action)();
         }
 
-        static void reDo() {
-            if (m_num_commands > m_high_water) {
-                std::cout << "Commands is full." << std::endl;
-                return;
-            }
-            (m_command_list[m_num_commands]->m_receiver->*(m_command_list[m_num_commands]->m_action))();
-            m_num_commands++;
-        }
-
         static void unDo() {
             if (0 == m_num_commands) {
-                std::cout << "No commands need to do." << std::endl;
+                std::cout << "There is nothing to undo at this point." << std::endl;
                 return;
             }
             m_command_list[m_num_commands - 1]->m_receiver->reinstateMemento(m_mem_list[m_num_commands - 1]);
             m_num_commands--;
+        }
+
+        static void reDo() {
+            if (m_num_commands > m_maxCommands || 0 == m_maxCommands) {
+                std::cout << "There is nothing to redo at this point." << std::endl;
+                return;
+            }
+            (m_command_list[m_num_commands]->m_receiver->*(m_command_list[m_num_commands]->m_action))();
+            m_num_commands++;
         }
     private:
         Number *m_receiver;
@@ -52,7 +56,7 @@ extern "C" {
         static Memento *m_mem_list[MAX_OPERATION];
         static Command *m_command_list[MAX_OPERATION];
         static int m_num_commands;
-        static int m_high_water;
+        static int m_maxCommands;
     };
 
 #ifdef  __cplusplus
